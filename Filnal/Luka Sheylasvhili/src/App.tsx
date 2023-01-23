8f48e75a1a53c3e6e9f35cc91f028b46ae315b84
luka sheylashvili <luka.sheylashvili@gmail.com>
2023-01-23 18:34:30 +0400

    final exal in Js-React-TSX :(

--- Filnal/Luka Sheylasvhili/src/App.tsx
+++ Filnal/Luka Sheylasvhili/src/App.tsx
@@ -9,38 +9,43 @@ interface Products{
   category: string,
   thumbnail: string,
   stock: number,
+  deleteProduct(id: number): any
 }
 function App() {
-  const URL = "https://dummyjson.com/products/search?q=phone"
-  const[trigger, setTrigger] = useState(0);
+  const[searchstring, setSearchstring] = useState("");
+  const URL = `https://dummyjson.com/products/search?q=${searchstring}`
   const[products, setProducts] = useState<Products[]>([]);
   const fetchProducts = async () => {
       const allProduct = await fetch(URL)
       const res = await allProduct.json()
-      setProducts(res.products);
-      console.log(products)
-      return res
+      const result = res.products;
+      setProducts(result);
   }
-
+  const deleteProduct = (id:number) => {
+      setProducts(products.filter(product => product.id != id));
+  }
   useEffect(() => {
       fetchProducts()
-  }, [trigger] )
+  }, [searchstring] )
 
   return (
         <main>
           <div className='header'>
-            <button onClick={() => setTrigger(trigger + 1)}>clickme</button>
+          <label>Search Product:</label>
+          <input type="text" onChange={(e) => setSearchstring(e.target.value)} value={searchstring}/>
           </div>
           <section className='itemsloop'>
             {products.map((product) => (
                   <SingleProduct
                           key={product.id}
+                          id={product.id}
                           title={product.title}
                           price={product.price}
                           description={product.description}
                           category={product.category}
                           stock={product.stock}
                           thumbnail={product.thumbnail}
+                          deleteProduct = {deleteProduct}
                   />
             ))}
           </section>
--- Filnal/Luka Sheylasvhili/src/components/product/SingleProduct.tsx
+++ Filnal/Luka Sheylasvhili/src/components/product/SingleProduct.tsx
@@ -1,20 +1,25 @@
 interface Products{
+    id: number,
     title: string,
     description: string,
     price: number,
     category: string,
     thumbnail: string,
     stock: number,
+    deleteProduct(id:number): any
   }
-const SingleProduct = ({title, price, description, category, stock, thumbnail}:Products) => {
+const SingleProduct = ({title, id, price, description, category, stock, thumbnail, deleteProduct}:Products) => {
     return(
-        <>
+        <div>
             <h1>{title}</h1>
-            <h2>{category}</h2>
-            <h3>{price}</h3>
-            <h4>{stock}</h4>
+            <img src={thumbnail}></img>
+            <h2>category: {category}</h2>
+            <h3>price: ${price}</h3>
+            <h4>in stock: {stock}</h4>
+            <p>description: </p>
             <h5>{description}</h5>
-        </>
+            <button onClick={() => deleteProduct(id)}>Delete</button>
+        </div>
     )
 }
 export default SingleProduct
